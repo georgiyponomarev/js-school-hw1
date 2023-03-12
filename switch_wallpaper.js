fb9894a0306fa251d6a8d33ea08763b1d46eb68b
georgiyponomarev <georgiy.ponomarev@quantori.com>
2023-03-12 13:20:04 +0400

    add cat pics

--- switch_wallpaper.js
+++ switch_wallpaper.js
@@ -2,7 +2,20 @@ var images = [
     "images/future_alien_invasion.jpg", 
     "images/galaxy.jpg", 
     "images/tbilisi.jpg",
-    "images/wallpaper.png"
+    "images/wallpaper.png",
+    "images/shishka.jpg",
+    "images/neural-shishka-1.jpg",
+    "images/neural-shishka-2.jpg",
+    "images/neural-shishka-3.jpg",
+    "images/neural-shishka-4.jpg",
+    "images/neural-shishka-5.jpg",
+    "images/neural-shishka-6.jpg",
+    "images/neural-shishka-7.jpg",
+    "images/neural-shishka-8.jpg",
+    "images/neural-shishka-9.jpg",
+    "images/neural-shishka-10.jpg",
+    "images/neural-shishka-11.jpg",
+    "images/neural-shishka-12.jpg",
 ]
 
 var currentIndex = 0;
